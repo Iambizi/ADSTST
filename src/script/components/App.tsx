8bf26d14b8fe07dafbf6b66c645f4ddfb78abc05
Iambizi <amirbzmn@gmail.com>
2021-06-27 13:57:28 -0400

    organizes styles and adds toggle functionality

--- src/script/components/App.tsx
+++ src/script/components/App.tsx
@@ -12,13 +12,8 @@ export const App: React.FC<dataProps> = () => {
 {/* MAIN VARIABLES FROM OUR JSON DATA */}
     const rawData = [{...coverageData}];
     const data = rawData[0];
-
-    // returns years
     const years = data.years;
-    // returns coverage
     const coverage = data.coverage;
-    // returns model keys from data.coverage
-    const coverageKey = Object.keys(data.coverage);
     const models = data['vehicle-models'];
     
     {/* LOGIC FOR DISPLAYING DATA ON THE FE */}
@@ -37,7 +32,7 @@ export const App: React.FC<dataProps> = () => {
             }
     },[])
 
-    // function that displays that updates JSON data and cell UI on click
+    // function that toggles cell on click, updates coverage data and updates UI
     const updateCoverage = (e) =>{
         
         const cellYear = e.target.getAttribute("data-coverage");
@@ -48,12 +43,21 @@ export const App: React.FC<dataProps> = () => {
         if(confirmCoverage === true){
             modelCoverage.slice(modelCoverage.length, cellYear);
             e.target.classList.remove("in"); 
+            const index = modelCoverage.indexOf(parseInt(cellYear));
+            if (index > -1) {
+                modelCoverage.splice(index, 1);
+            }
+            // log new coverage array sorted in an ascending order
+            console.log(cellModel);
+            console.log(modelCoverage.sort((a, b) => b - a));   
         }
         else{
             modelCoverage.push(parseInt(cellYear)); 
             e.target.classList.add("in");
         }
-        console.log(modelCoverage.sort((a, b) => b - a));               
+            // log new coverage array sorted in an ascending order
+            console.log(cellModel);
+            console.log(modelCoverage.sort((a, b) => b - a));               
     }
     
     // returns each cell by iterating over the years so correct # of cells is returned per row
@@ -68,7 +72,7 @@ export const App: React.FC<dataProps> = () => {
             <table>
                 <thead>
                     <tr>
-                        <th className="logo"></th>
+                        <th className="logo"><img src={"../../images/logo.png"} alt="logo"/></th>
                         {data.years.map((year, i)=>(
                             <td data-year={`${years[i]}`} className="year" key={i}>{years[i]}</td>
                             )
@@ -78,7 +82,7 @@ export const App: React.FC<dataProps> = () => {
                 <tbody>
                     {/* generate left row of vehicle models  */}
                     {models.map((item, i)=>(
-                        <tr data-model={coverageKey[i]} className="bodyRow" key={i}>
+                        <tr data-model={models[i]} className="bodyRow" key={i}>
                             <td className="model">{models[i]}</td>
                             {/* then over years to generate coverage rows */}
                              {getRowsData()}
