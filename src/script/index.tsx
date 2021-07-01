777edbc3cf75df04d59ac8e061ee36d2f2a0017c
Iambizi <amirbzmn@gmail.com>
2021-06-30 23:12:53 -0400

    adds missing prop

--- src/script/index.tsx
+++ src/script/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
-
+import coverageData from "../data/coverage.json";
 import { App } from './components/App';
 
-ReactDom.render(<App />, document.querySelector('#app'));
+ReactDom.render(<App data={coverageData} />, document.querySelector('#app'));
